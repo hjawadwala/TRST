67e301ec4591056bc087b160faaa871fa035e95b
Hussain Jawadwala <hjawadwala@zolute.co>
2026-02-28 13:17:25 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,8 +8,10 @@ const app = express();
 
 //Middlewares
 app.use(cors({
-    origin: '*',
-    credentials: false,
+    origin: function(origin, callback) {
+        callback(null, true);
+    },
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
